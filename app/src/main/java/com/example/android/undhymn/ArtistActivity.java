6b50962411836e107939ef2f93fc0e633399bd8c
Nishant Patel <nishantuwindsor@gmail.com>
2017-10-28 22:05:59 -0400

    Create custom adapter for artist name and photo

--- app/src/main/java/com/example/android/undhymn/ArtistActivity.java
+++ app/src/main/java/com/example/android/undhymn/ArtistActivity.java
@@ -2,6 +2,9 @@ package com.example.android.undhymn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ArtistActivity extends Activity {
 
@@ -9,5 +12,23 @@ public class ArtistActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
+
+        // Create an arrayList of an object TrackDetail
+        ArrayList<TrackDetail> artistDetail = new ArrayList<>();
+        // Add details about artist in ArrayList
+        artistDetail.add(new TrackDetail("Akcent", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Avicii", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Blue Swede", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "The Clash", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Dreamer", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Eminem", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "The Script", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Ed Sheeran", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Marshmello", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Zyan Feat. Sia", R.drawable.artist_placeholder));
+
+        ListView listView = findViewById(R.id.artist_list);
+        ArtistAdapter artistAdapter = new ArtistAdapter(this, artistDetail);
+        listView.setAdapter(artistAdapter);
     }
 }
---
+++ app/src/main/java/com/example/android/undhymn/ArtistAdapter.java
@@ -0,0 +1,51 @@
+package com.example.android.undhymn;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Nishant on 10/28/2017.
+ */
+
+public class ArtistAdapter extends ArrayAdapter<TrackDetail> {
+
+    /* Context of App */
+    private Context mContext;
+
+    public ArtistAdapter(@NonNull Context context, @NonNull List<TrackDetail> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.artist_list_item, parent, false);
+        }
+
+        // Get the current list item
+        TrackDetail currentItem = getItem(position);
+
+        // Find the id for artist photo ImageView in artist_list_item.xml and set the image
+        ImageView artistPhoto = listItemView.findViewById(R.id.artist_list_item_image);
+        artistPhoto.setImageResource(currentItem.getArtistPhoto());
+
+        // Find the id for artist name TextView in artist_list_item.xml and set the name of artist
+        TextView artistName = listItemView.findViewById(R.id.artist_list_item_name);
+        artistName.setText(currentItem.getArtistName().toString());
+
+        return listItemView;
+    }
+}
