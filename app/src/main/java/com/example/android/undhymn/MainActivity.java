5057145dff90035588bf6f884899f386a5a203f6
Nishant Patel <nishantuwindsor@gmail.com>
2017-10-28 22:10:18 -0400

    Attach a listener to Artist and add action when clicked on

--- app/src/main/java/com/example/android/undhymn/MainActivity.java
+++ app/src/main/java/com/example/android/undhymn/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find textView with id main_songs_activity_link
+        // Find TextView with id main_songs_activity_link
         TextView songsTextView = findViewById(R.id.main_songs_activity_link);
         // Attach a listener to  textView to listen to any touch event
         songsTextView.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Find textView with id main_playlist_activity_link
+        // Find TestView with id main_playlist_activity_link
         TextView playlistTextView = findViewById(R.id.main_playlist_activity_link);
         // Attach a listener to  textView to listen to any touch event
         playlistTextView.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Find textView with id main_album_activity_link
+        // Find TextView with id main_album_activity_link
         TextView albumTextView = findViewById(R.id.main_album_activity_link);
         // Attach a listener to  textView to listen to any touch event
         albumTextView.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
                  * to {@link AlbumActivity}
                  */
                 Intent intent = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find TextView with id main_artist_activity_link
+        TextView artistTextView = findViewById(R.id.main_artist_activity_link);
+        // Attach a listener to  textView to listen to any touch event
+        artistTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * When user clicks the Artist, then start a new intent to navigate
+                 * to {@link ArtistActivity}
+                 */
+                Intent intent = new Intent(MainActivity.this, ArtistActivity.class);
                 startActivity(intent);
             }
         });
