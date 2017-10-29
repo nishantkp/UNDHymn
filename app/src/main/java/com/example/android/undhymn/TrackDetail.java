1cf20cd3bec8de354bc6ffba5aafcbc36730d782
Nishant Patel <nishantuwindsor@gmail.com>
2017-10-28 21:47:30 -0400

    Add spacing to comments in TrackDetail class

--- app/src/main/java/com/example/android/undhymn/TrackDetail.java
+++ app/src/main/java/com/example/android/undhymn/TrackDetail.java
@@ -24,11 +24,12 @@ public class TrackDetail {
 
     /***
      *  Public constructor for detail of songs
+     *
      * @param songName name of song
      * @param artistName name of artist
      * @param albumArt drawable resource id for album art
      */
-    public TrackDetail(String songName, String artistName, int albumArt){
+    public TrackDetail(String songName, String artistName, int albumArt) {
         mSongName = songName;
         mArtistName = artistName;
         mAlbumArt = albumArt;
@@ -36,64 +37,72 @@ public class TrackDetail {
 
     /***
      *  Public constructor for detail of album
+     *
      * @param albumName name of the album
      * @param songName name of song
      * @param artistName name of artist
      * @param albumArt drawable resource id for album art
      */
-    public TrackDetail(String albumName,String songName, String artistName, int albumArt){
+    public TrackDetail(String albumName, String songName, String artistName, int albumArt) {
         mAlbumName = albumName;
         mSongName = songName;
         mArtistName = artistName;
         mAlbumArt = albumArt;
     }
+
     /***
      *  Public constructor for detail of artist
+     *
      * @param artistName name of artist
      * @param artistPhoto drawable resource id for photo of the artist
      */
-    public TrackDetail(String artistName, int artistPhoto){
+    public TrackDetail(String artistName, int artistPhoto) {
         mArtistName = artistName;
         mAristPhoto = artistPhoto;
     }
 
     /**
      * This method is called to get name of the album
+     *
      * @return name of the album
      */
-    public String getAlbumName(){
+    public String getAlbumName() {
         return mAlbumName;
     }
 
     /**
-     *  This method is called to get name of the song
+     * This method is called to get name of the song
+     *
      * @return name of song
      */
-    public String getSongName(){
+    public String getSongName() {
         return mSongName;
     }
 
     /**
      * This method is called to get name of artist
+     *
      * @return name of artist
      */
-    public String getArtistName(){
+    public String getArtistName() {
         return mArtistName;
     }
 
     /**
      * This method is called to get resource id for drawable album art
+     *
      * @return resource id of drawable album art
      */
-    public  int getAlbumArt(){
+    public int getAlbumArt() {
         return mAlbumArt;
     }
 
     /**
      * This method is called to get resource id for artist photo
+     *
      * @return resource id for photo of the artist
      */
-    public  int getArtistPhoto(){
+    public int getArtistPhoto() {
         return mAristPhoto;
     }
 }
