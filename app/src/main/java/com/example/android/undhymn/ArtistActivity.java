785f6657ba228e95811bf190ce4ca7c14a6aac80
Nishant Patel <nishantuwindsor@gmail.com>
2017-10-29 02:27:01 -0400

    Add artist images and update artist activity accordingly

--- app/src/main/java/com/example/android/undhymn/ArtistActivity.java
+++ app/src/main/java/com/example/android/undhymn/ArtistActivity.java
@@ -16,16 +16,16 @@ public class ArtistActivity extends Activity {
         // Create an arrayList of an object TrackDetail
         ArrayList<TrackDetail> artistDetail = new ArrayList<>();
         // Add details about artist in ArrayList
-        artistDetail.add(new TrackDetail("Akcent", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Avicii", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Blue Swede", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "The Clash", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail("Akcent", R.drawable.akcent_artist));
+        artistDetail.add(new TrackDetail( "Avicii", R.drawable.avicii_artist));
+        artistDetail.add(new TrackDetail( "Blue Swede", R.drawable.blue_swede_artist));
+        artistDetail.add(new TrackDetail( "The Clash", R.drawable.the_clash_artist));
         artistDetail.add(new TrackDetail( "Dreamer", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Eminem", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "The Script", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Ed Sheeran", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Marshmello", R.drawable.artist_placeholder));
-        artistDetail.add(new TrackDetail( "Zyan Feat. Sia", R.drawable.artist_placeholder));
+        artistDetail.add(new TrackDetail( "Eminem", R.drawable.eminem_not_afraid));
+        artistDetail.add(new TrackDetail( "The Script", R.drawable.the_script_artist));
+        artistDetail.add(new TrackDetail( "Ed Sheeran", R.drawable.ed_sheeran_artist));
+        artistDetail.add(new TrackDetail( "Marshmello", R.drawable.marshmellow_dj));
+        artistDetail.add(new TrackDetail( "Zyan Feat. Sia", R.drawable.zyan_artist));
 
         ListView listView = findViewById(R.id.artist_list);
         ArtistAdapter artistAdapter = new ArtistAdapter(this, artistDetail);
